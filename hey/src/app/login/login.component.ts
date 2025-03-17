bdd87f24c553908ddaf39fd922595ae2750f7724
Kaushal-kurhe <kurhekaushal@gmail.com>
2025-03-17 16:20:15 +0530

    changes updated

--- hey/src/app/login/login.component.ts
+++ hey/src/app/login/login.component.ts
@@ -44,7 +44,7 @@ export class LoginComponent implements OnInit{
   }
    onLogin(){
     
-    debugger
+    // debugger
     const isUserExist = this.signupUsers.find(m => m.userName == this.loginobj.userName && m.password == this.loginobj.password);
     if (isUserExist != undefined){
       alert('User Login Successfull');
