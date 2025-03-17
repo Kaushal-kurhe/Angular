b84e17d97dbe2928d48845f4379c9c0260c4e0d6
Kaushal-kurhe <kurhekaushal@gmail.com>
2025-03-17 11:40:33 +0530

    Initial commit

---
+++ hey/src/app/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { json } from 'stream/consumers';
+import { FormsModule } from '@angular/forms';
+// import { Router } from 'express';
+import { Router } from '@angular/router'; 
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit{
+   
+  signupUsers : any[] = [];
+  signupObj : any = {
+  userName : '',
+  email : '',
+  password : ''
+  };
+  loginobj : any = {
+    userName : '',
+    password : '',
+    email : ''
+
+  }
+   constructor(private router: Router){}
+
+   ngOnInit(): void {
+     const localData = localStorage.getItem('signupUsers')
+     if(localData != null )
+      this.signupUsers = JSON.parse(localData); 
+   }
+   onSignup(){
+   this.signupUsers.push(this.signupObj)
+   localStorage.setItem('signUpUsers', JSON.stringify(this.signupUsers))
+  this.signupObj = {
+  userName : '',
+  email : '',
+  password : ''
+   };
+  }
+   onLogin(){
+    
+    
+    const isUserExist = this.signupUsers.find(m => m.userName == this.loginobj.userName && m.password == this.loginobj.password);
+    if (isUserExist != undefined){
+      alert('User Login Successfull');
+      this.router.navigate(['movie']);
+    }
+      else {
+        alert('Wrong credentials');
+      }
+   }
+
+}
